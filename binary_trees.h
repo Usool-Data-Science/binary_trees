93f1639f12f05f6600ace1dcea9c568dba1868e3
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-26 16:18:46 +0100

    Add all main files

---
+++ binary_trees.h
@@ -0,0 +1,33 @@
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
+
+#include <stddef.h>
+
+
+/*********** Basic Binary Tree ************************/
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+/*Binary Search Tree*/
+typedef struct binary_tree_s binary_tree_t;
+/*AVL Tree*/
+typedef struct binary_tree_s avl_t;
+/*Max Binary Heap*/
+typedef struct binary_tree_s heap_t;
+
+/*Binary tree print function prototype*/
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* _BINARY_TREES_H */
