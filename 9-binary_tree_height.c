513ef56d5b3ddaaaadd2d6db7758daa8a61a2ce9
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-27 17:47:13 +0100

    Solve up to task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+  * binary_tree_height - Finds the height of the tree.
+  * @tree: The tree whose height we want to find.
+  *
+  * Return: Height of the tree;
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+	return ((left_height > right_height ? left_height : right_height));
+}
