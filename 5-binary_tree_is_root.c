9c6ae3456dfb63fa3800e54997e9e4e6f6877358
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-28 18:48:39 +0100

    Mod 5-bin

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -14,11 +14,9 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if
-	(
-		(node->left != NULL || node->right != NULL) &&
-		(node->parent == NULL)
-	)
+	if (
+			(node->left != NULL || node->right != NULL) &&
+			node->parent == NULL)
 		return (1);
 	else
 		return (0);
