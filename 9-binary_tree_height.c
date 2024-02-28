2f4f8e31548103c531edbd8389006a8ecdec31ad
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-28 19:27:16 +0100

    Mod - fix verbosity, bug

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
@@ -16,5 +16,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
-	return ((left_height > right_height ? left_height : right_height));
+	return ((left_height > right_height) ? left_height : right_height);
 }
