684df77f6bcf23e72831e1b656f5b3f46927cddb
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-28 20:47:22 +0100

    update task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,8 +21,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return (
 		((binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		&& (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
-		&& (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right))) ? 1 : 0
+		) ? 1 : 0
 		);
 }
 
