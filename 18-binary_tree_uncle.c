7a2fea5b6a6a79978a733f98736f0b6600d7c79d
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-28 22:25:52 +0100

    Add find tree uncle function

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,8 +7,8 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    return (node && node->parent && node->parent->parent) ?
-           (node->parent->parent->left == node->parent ?
-            node->parent->parent->right : node->parent->parent->left) : NULL;
+	return ((node && node->parent && node->parent->parent)
+	? (node->parent->parent->left == node->parent
+	? node->parent->parent->right : node->parent->parent->left) : NULL);
 }
 
