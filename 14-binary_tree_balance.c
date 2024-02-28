c844f997e45178aa1171880e044fec835c20c7fd
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-28 21:37:42 +0100

    Mod - fix verbosity, bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,3 +22,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 }
 
 
+/**
+  * binary_tree_height - Finds the height of the tree.
+  * @tree: The tree whose height we want to find.
+  *
+  * Return: Height of the tree;
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+	return ((left_height >= right_height) ? left_height : right_height);
+}
