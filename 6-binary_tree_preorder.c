3ee32d3724019d59898b231b49e5be8491b38bf1
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-02-28 23:37:26 +0100

    Update task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,11 +10,11 @@
   */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-		return;
-	printf("%d\n", tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
-
+	if (tree && func)
+	{
+		printf("%d\n", tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 	return;
 }
